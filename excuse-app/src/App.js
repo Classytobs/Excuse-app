ed0908c66dcef064df2261d1de57f85c8b48ee21
classytobs <adewumitobi20@gmail.com>
2023-01-15 01:07:02 +0100

    installed axios and made use of it to complete the task

--- excuse-app/src/App.js
+++ excuse-app/src/App.js
@@ -1,7 +1,14 @@
 import { useState } from "react";
+import Axios from 'axios';
 
 function App() {
 const [excuse, setExcuse] = useState('')
+
+const fetchExcuse = (excuse) => {
+  Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`).then((res) => {
+    setExcuse(res.data[0].excuse)
+  })
+}
 
   return (
     <div className="flex flex-col items-center content-center justify-center">
@@ -10,12 +17,17 @@ const [excuse, setExcuse] = useState('')
       </div>
       <div className="flex flex-col">
         <button className="bg-slate-500 hover:bg-slate-600 
-        px-3 py-2 rounded-md">Party</button>
+        px-3 py-2 rounded-md"
+        onClick={()=>fetchExcuse('party')}>Party</button>
         <button className="bg-slate-500 hover:bg-slate-600 
-        px-3 py-2 rounded-md">Family</button>
+        px-3 py-2 rounded-md"
+        onClick={()=>fetchExcuse('family')}>Family</button>
         <button className="bg-slate-500 hover:bg-slate-600 
-        px-3 py-2 rounded-md">Office</button>
+        px-3 py-2 rounded-md"
+        onClick={()=>fetchExcuse('office')}>Office</button>
       </div>
+
+      <p className="px-8 mt-8">{excuse}</p>
     </div>
   );
 }
